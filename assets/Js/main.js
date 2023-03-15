0a948573fe907b864e797c3d96be3b2bab598cb7
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-15 16:48:28 +0100

    shopping list array

---
+++ assets/Js/main.js
@@ -0,0 +1,13 @@
+/* 
+Data una lista della spesa,
+stampare sulla pagina (anche brutalmente, basta che si vedano)
+gli elementi della lista individualmente con un ciclo while.
+
+Consigli:
+Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
+*/
+
+// creo lista 
+const shoppingList = ["vinellino", "cibo gatti", "biscottini vegani", "Pasta", "Birra vegana", "salami", "Burro", "acido", "disinfettante cesso", "veleno per topi"];
+
